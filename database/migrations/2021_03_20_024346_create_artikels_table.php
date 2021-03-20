1bd7d72e78accb292b81a3ea3b3e766d48374144
Zaenury Dhany Wibowo <zaenury5868@gmail.com>
2021-03-20 13:35:16 +0700

    layouting autentikasi

---
+++ database/migrations/2021_03_20_024346_create_artikels_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArtikelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artikels', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('judul');
+            $table->string('slug', 100);
+            $table->longText('isi');
+            $table->string('kategori', 50);
+            $table->string('img');
+            $table->integer('view')->nullable();
+            $table->string('video')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artikels');
+    }
+}
---
+++ database/migrations/2021_03_20_024406_create_komentars_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKomentarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('komentars', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('komentar');
+            $table->string('img')->nullable();
+            $table->integer('induk')->nullable();
+            $table->unsignedBigInteger('artikel_id');
+            $table->foreign('artikel_id')->references('id')->on('artikels')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('komentars');
+    }
+}
